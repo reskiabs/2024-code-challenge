74dfa949e25948f3da86d8f8d8b3db6d809d2090
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2024-01-30 11:47:31 +0800

    day #5/366

---
+++ 5-zeroFuel.js
@@ -0,0 +1,15 @@
+// WPU Coding challenge 2024
+// 5/366 on 29/01/2024
+
+// const zeroFuel = (distanceToPump, mpg, fuelLeft) => {
+//   if (mpg * fuelLeft >= distanceToPump) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+const zeroFuel = (distanceToPump, mpg, fuelLeft) =>
+  fuelLeft * mpg >= distanceToPump;
+
+console.log(zeroFuel(50, 25, 2));
+console.log(zeroFuel(100, 50, 1));
