c90a91e8c965869e73135f07a9c91d20f1ad7fcf
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2024-05-01 13:46:41 +0800

    27/366 on 01/05/2024

---
+++ 27-setAlarm.js
@@ -0,0 +1,25 @@
+// WPU Coding challenge 2024
+// 26/366 on 01/05/2024
+// https://www.codewars.com/kata/568dcc3c7f12767a62000038
+
+function setAlarm(employed, vacation) {
+  if (employed) {
+    if (vacation) {
+      return false;
+    } else {
+      return true;
+    }
+  } else {
+    if (vacation) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+}
+
+const setAlarm2 = (employed, vacation) => employed && !vacation;
+
+console.log(setAlarm2(true, true));
+console.log(setAlarm2(false, true));
+console.log(setAlarm2(true, false));
