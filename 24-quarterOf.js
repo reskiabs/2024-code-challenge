b251449caa8e46f827a7899b622ec8b57c53ed12
Reski Abbas <reskiabbas@student.unismuh.ac.id>
2024-05-01 13:01:06 +0800

    24/366 on 01/05/2024

---
+++ 24-quarterOf.js
@@ -0,0 +1,22 @@
+// WPU Coding challenge 2024
+// 24/366 on 01/05/2024
+// https://www.codewars.com/kata/5ce9c1000bab0b001134f5af
+
+const quarterOf = (month) => {
+  if (month <= 3) {
+    return 1;
+  } else if (month <= 6) {
+    return 2;
+  } else if (month <= 9) {
+    return 3;
+  } else if (month <= 12) {
+    return 4;
+  }
+};
+
+const quarterOf2 = (month) =>
+  month <= 3 ? 1 : month <= 6 ? 2 : month <= 9 ? 3 : month <= 12 ? 4 : null;
+
+console.log(quarterOf(1));
+console.log(quarterOf(8));
+console.log(quarterOf(11));
